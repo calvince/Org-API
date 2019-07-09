8151ae4f39099d1462ee0602feb05d2f244d2fb3
calvince <calmosh1421@gmail.com>
2019-07-09 07:51:05 +0300

    Implements various interfaces

---
+++ src/main/java/dao/EmployeesDao.java
@@ -0,0 +1,22 @@
+package dao;
+
+import models.Department;
+import models.Employees;
+
+import java.util.List;
+
+public interface EmployeesDao {
+    void add(Employees employees);
+
+    //Find a user by the id
+    Employees findById(int id);
+
+    //Get all users
+    List<Employees> getAllEmployees();
+
+    //Add a user to a department
+    void addEmployeeToDepartment(Employees employees, Department department);
+
+    //Get all departments Employee belongs to
+    List<Department> getAllDepartmentsForEmployee(int employee_id);
+}
