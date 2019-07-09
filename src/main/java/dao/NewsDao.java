a584d7c20fa6c8043500025db90668952aa4c91d
calvince <calmosh1421@gmail.com>
2019-07-09 07:52:46 +0300

    Contains various interface implementations

---
+++ src/main/java/dao/NewsDao.java
@@ -0,0 +1,19 @@
+package dao;
+
+import models.News;
+
+import java.util.List;
+
+public interface NewsDao {
+    //Add new for a department
+    void add(News news);
+
+    //Find news by the id
+    News findById(int id);
+
+    //Update department news
+    void updateNews(int id, String title, String content);
+
+    //Get all department news by department id
+    List<News> getAllNews(int departmentId);
+}
