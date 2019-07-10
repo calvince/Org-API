7dba1eabe1eba26a43ef8b0c175ac39b9b4cc7a5
calvince <calmosh1421@gmail.com>
2019-07-10 06:59:20 +0300

    this commit will implement department database actions

--- src/main/java/dao/DepartmentDao.java
+++ src/main/java/dao/DepartmentDao.java
@@ -2,6 +2,7 @@ package dao;
 
 import models.Department;
 import models.Employees;
+import models.News;
 
 import java.util.List;
 
@@ -17,12 +18,14 @@ public interface DepartmentDao {
     //Get all news for a department
     List<News> getAllNews(int departmentId);
 
+    //many to many relationship
     //Add a department to a user
     void addDepartmentToEmployees(Department department, Employees employees);
-
+    //many to many relationship
     //Get all users in a particular department
     List<Employees> getAllEmployeesForADepartment(int employee_id);
 
     //delete
+    //deleteById(int id);
     void clearAll();
 }
