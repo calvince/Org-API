510ce6c48ee7a936d30fab786bf5b3757cd4a7b4
calvince <calmosh1421@gmail.com>
2019-07-10 07:00:24 +0300

    this commit will import News model

--- src/main/java/dao/Sql2oDepartmentDao.java
+++ src/main/java/dao/Sql2oDepartmentDao.java
@@ -2,6 +2,7 @@ package dao;
 
 import models.Department;
 import models.Employees;
+import models.News;
 import org.sql2o.Connection;
 import org.sql2o.Sql2o;
 import org.sql2o.Sql2oException;
@@ -10,13 +11,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Sql2oDepartmentDao implements DepartmentDao {
+
     private final Sql2o sql2o;
     public  Sql2oDepartmentDao(Sql2o sql2o){
         this.sql2o = sql2o;
     }
     @Override
     public void add(Department department) {
-        String sql = "INSERT INTO departments (department, description, number_employees) VALUES (:department, :description, :number_employees)";
+        String sql = "INSERT INTO departments (department, description,  number_employees) VALUES (:department, :description, :number_employees)";
         try(Connection conn = sql2o.open()){
             int id = (int) conn.createQuery(sql, true)
                     .bind(department)
@@ -48,7 +50,7 @@ public class Sql2oDepartmentDao implements DepartmentDao {
 
     @Override
     public List<News> getAllNews(int departmentId) {
-        String sql = "SELECT * FROM news WHERE departmentId =:departmentId";
+        String sql = "SELECT * FROM news WHERE departmentId=:departmentId";
         try(Connection conn = sql2o.open()){
             return conn.createQuery(sql).addParameter("departmentId", departmentId)
                     .throwOnMappingFailure(false)
@@ -56,13 +58,14 @@ public class Sql2oDepartmentDao implements DepartmentDao {
         }
     }
 
+        //many to many relationships
     @Override
     public void addDepartmentToEmployees(Department department, Employees employees) {
-        String sql = "INSERT INTO departments_users(department_id, user_id) VALUES (:department_id, :user_id)";
+        String sql = "INSERT INTO departments_employees(departmentId, employeeId) VALUES (:departmentId, :employeeId)";
         try(Connection conn = sql2o.open()){
             conn.createQuery(sql)
-                    .addParameter("department_id", department.getId())
-                    .addParameter("user_id", employees.getId())
+                    .addParameter("departmentId", department.getId())
+                    .addParameter("employeeId", employees.getId())
                     .executeUpdate();
         }catch (Sql2oException ex){
             System.out.println(ex);
@@ -70,17 +73,17 @@ public class Sql2oDepartmentDao implements DepartmentDao {
     }
 
     @Override
-    public List<Employees> getAllEmployeesForADepartment(int department_id) {
+    public List<Employees> getAllEmployeesForADepartment(int departmentId) {
         ArrayList<Employees> allUsers = new ArrayList<>();
-        String matchToGetTheUserIds = "SELECT employee_id FROM departments_users WHERE department_id =:department_id";
+        String matchToGetTheUserIds = "SELECT employeeId FROM departments_employees WHERE departmentId =:departmentId";
         try(Connection conn = sql2o.open()){
             List<Integer> allEmployeesIds = conn.createQuery(matchToGetTheUserIds)
-                    .addParameter("department_id", department_id)
+                    .addParameter("departmentId", departmentId)
                     .executeAndFetch(Integer.class);
-            for(Integer employee_id: allEmployeesIds){
-                String getFromUsers = "SELECT * FROM employees WHERE id=:employee_id";
+            for(Integer employeeId: allEmployeesIds){
+                String getFromUsers = "SELECT * FROM employees WHERE id=:employeeId";
                 allUsers.add(conn.createQuery(getFromUsers)
-                        .addParameter("employee_id", employee_id)
+                        .addParameter("employeeId", employeeId)
                         .executeAndFetchFirst(Employees.class));
             }
         }catch (Sql2oException ex){
